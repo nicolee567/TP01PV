ac6eb1c5108c06a301a7c35d10453fcaf43f766f
nicolee567 <nicole.mendivil.cabrera@gmail.com>
2024-04-04 15:32:42 -0300

    Se agrega paquete clase para el ejercicio

---
+++ TP01_MendivilCabreraNicole/src/ar/edu/unju/fi/Principal.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+
+	}
+
+}
---
+++ TP01_MendivilCabreraNicole/src/ar/edu/unju/fi/ejercicio01/Main.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.ejercicio01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero01= 5;
+		int numero02 =6;
+		double promedio =(double)(numero01 + numero02)/2;
+		System.out.println("El promedio es: "+ promedio);
+
+	}
+
+}
